033e87932559790f178bd4d553629b5a155b1fb6
Muhamad Ahad <muhamad.ahad.8@gmail.com>
2020-05-07 10:00:31 +0700

    amenities filter

--- src/config/API.js
+++ src/config/API.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const API = axios.create({
-  // baseURL: "https://housyahad.herokuapp.com/api/v1",
-  baseURL: "http://localhost:4000/api/v1",
+  baseURL: "https://housyahad.herokuapp.com/api/v1",
+  //baseURL: "http://localhost:4000/api/v1",
 });
 
 export const setAuthToken = (token) => {
